66f4e5210a065a34698b6bd87f10f72df5d10afa
moondogg71 <richardstiehm@gmail.com>
2016-04-28 11:52:26 +0000

    Submit Form Passing To Confirm page

--- routes/clients/index.js
+++ routes/clients/index.js
@@ -12,6 +12,31 @@ router.get('/', function(req, res) {
    res.render('index'); 
 });
 
+
+router.post('/post', function(req, res) {
+    var inputDefault = "false";
+    var code = req.body.code ? req.body.code : inputDefault;;
+    var checkbox1 = req.body.checkbox1 ? req.body.checkbox1 : inputDefault;
+    var checkbox2 = req.body.checkbox2 ? req.body.checkbox2 : inputDefault;
+    var checkbox3 = req.body.checkbox3 ? req.body.checkbox3 : inputDefault;
+    var notes = req.body.notes ? req.body.notes : inputDefault;
+    
+    var orderInvoice = {
+        code: code,
+        checkbox1: checkbox1,
+        checkbox2: checkbox2,
+        checkbox3: checkbox3,
+        notes: notes
+    }
+    // console.log(code);
+    // console.log(checkbox1);
+    // console.log(checkbox2);
+    // console.log(checkbox3);
+    // console.log(notes);
+   res.render('confirm', {orderInvoice: orderInvoice});
+});
+
+
 //SHOW REGISTRATION FORM--------------------------------------------------------
 // router.get('/register', function(req, res) {
 //   res.render('register'); 
