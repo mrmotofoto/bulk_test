ba2fe1281fdf495ccbdac8c09dcbdd81090be08d
moondogg71 <richardstiehm@gmail.com>
2016-05-01 16:23:14 +0000

    ADD DATABASEURL

--- app.js
+++ app.js
@@ -2,23 +2,20 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
-//var passport = require('passport');
-//var LocalStrategy = require('passport-local');
 var Order = require('./models/order');
 var methodOverride = require('method-override');
-
+var orderRoutes = require('./routes/orders');
+//var passport = require('passport');
+//var LocalStrategy = require('passport-local');
 // var User = require('./models/user');
 //var seedDB = require('./seeds');
 //var methodOverride = require('method-override');
 
 
-//mongoose.connect('mongodb://localhost/bulk_upload_test');
-mongoose.connect('mongodb://rjs:harry113@ds029595.mlab.com:29595/bulk_upload');
+mongoose.connect(process.env.DATABASEURL);
 
 
-//REQUIRING Routes--------------------------------------------
 
-var orderRoutes = require('./routes/orders');
 
 
 app.use(bodyParser.urlencoded({extended: true}));
@@ -26,7 +23,6 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(orderRoutes);
-
 
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log('Server Is Running'); 
